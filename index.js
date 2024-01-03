0ea7784d9b6ae7a0348a4fdba1d3845c2367f87b
unknown <atharv21240@iiitd.ac.in>
2024-01-03 11:54:10 +0530

    jwt authentication

--- index.js
+++ index.js
@@ -2,22 +2,36 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
 const cors=require('cors');
+const jwt=require('jsonwebtoken');
 app.use(bodyParser.json());
 app.use(cors());
 
+const superkey='supra';
 
 const ADMINS=[]
 const USERS=[]
 const COURSES=[]
 
 function authenticateAdmin(req,res,next){
-    const {username,password}=req.headers;
-    const adminfound=ADMINS.find(a=>a.username==username&&a.password==password);
-    if(adminfound){
+    const {authorization}=req.headers;
+    const token=authorization.split(' ')[1];
+    jwt.verify(token,superkey,(err,data)=>{
+        if(err){
+            res.status(403).send('authentication failed');
+        }
         next();
-    }else{
-        res.status(403).send('authentication failed');
-    }
+    })
+}
+
+function userAuthentication(req,res,next){
+    const {authorization}=req.headers;
+    const token=authorization.split(' ')[1];
+    jwt.verify(token,superkey,(err,data)=>{
+        if(err){
+            res.status(403).send('authentication failed');
+        }
+        next();
+    });
 }
 
 
@@ -28,11 +42,23 @@ app.post('/admin/signup',(req,res)=>{
     const user={username:body.username,
     password:body.password}
     ADMINS.push(user);
-    res.send('admin created successfully');
+    const payload={username:body.username};
+    const token= jwt.sign(payload,superkey,{expiresIn:'1h'});
+    res.send({message:'admin created successfully',token:token});
 })
 
-app.post('/admin/login',authenticateAdmin,(req,res)=>{
-    res.send('logged in successfully');
+app.post('/admin/login',(req,res)=>{
+    const {username,password}=req.headers;
+    const admin=ADMINS.find(a=>a.username==username&&a.password==password);
+    
+    if(admin){
+        const payload={username:admin.username};
+    const token= jwt.sign(payload,superkey,{expiresIn:'1h'});
+    
+        res.send({message:'logged in successfully',token});
+    }else{
+        res.status(403).send('login failed');
+    }
 })
 var ctr=1;
 app.post('/admin/courses',authenticateAdmin,(req,res)=>{
@@ -41,10 +67,11 @@ app.post('/admin/courses',authenticateAdmin,(req,res)=>{
     description:courseBody.description,price:courseBody.price,
     publicshed:true, imageLink:courseBody.link
 }
-ctr++;
+    ctr++;
     COURSES.push(course);
     res.send({message:'course created successfully', id:(ctr-1)})
 })
+
 app.get('/admin/courses',authenticateAdmin,(req,res)=>{
     res.send(COURSES);
 })
@@ -65,11 +92,22 @@ app.post('/users/signup',(req,res)=>{
     const userBody=req.body;
     const user={username:userBody.username,password:userBody.password};
     USERS.push(user);
-    res.send('user account created successfully');
+    const payload={username:userBody.username}
+    const token=jwt.sign(payload,superkey,{expiresIn:'1h'});
+    res.send({message:'user created',token:token});
 });
 
-app.post('/users/login',userAuthentication,(req,res)=>{
-    res.send('user logged in successfully');
+app.post('/users/login',(req,res)=>{
+    const {username,password}=req.headers;
+    const user=USERS.find(a=>a.username==username&&a.password==password);
+    if(user){
+        const payload={username:user.username};
+    const token= jwt.sign(payload,superkey,{expiresIn:'1h'});
+    
+        res.send({message:'logged in successfully',token});
+    }else{
+        res.status(403).send('login failed');
+    }
 });
 
 app.get('/users/courses',userAuthentication,(req,res)=>{
@@ -92,5 +130,5 @@ app.post('/user/courses/:courseId',userAuthentication,(req,res)=>{
 
 
 app.listen(3002,()=>{
-    console.log('listening on port 3000');
+    console.log('listening on port 3002');
 })
