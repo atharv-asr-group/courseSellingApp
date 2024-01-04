f7890e9b212bbff5df24b38bddbbbcffdb48a85f
unknown <atharv21240@iiitd.ac.in>
2024-01-04 19:02:58 +0530

    correct way to get username and password useState

--- frontend/frontend/src/Signup.jsx
+++ frontend/frontend/src/Signup.jsx
@@ -1,29 +1,37 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
+import { useState } from 'react';
 
 function Signup(){
+    const [email, setEmail]=useState('');
+    const [password, setPassword]=useState('');
     return (
         <div>
+            {email}
         <center>
             <div style={{paddingTop:150}}>
                 <h4>Welcome to coursera. Sign up below.</h4>
             </div>
             <Card variant="outlined" style={{width:400, padding:20}}>
             <br />
-      <TextField fullWidth={true} id="username" label="username" variant="outlined" />
+      <TextField fullWidth={true} id="username" label="username" variant="outlined" 
+      onChange={(e)=>{
+        setEmail(e.target.value);
+    }}/>
             <br />
-            <TextField fullWidth={true} id="password" label="password" variant="outlined" />
+            <TextField fullWidth={true} id="password" label="password" variant="outlined" 
+            onChange={(e)=>{
+                setPassword(e.target.value);
+            }}/>
             <br /><br />
             <Button variant="contained"
              onClick={()=>{
-                let username=document.getElementById('username').value;
-                let password=document.getElementById('password').value;
-                console.log(username);
+                
                 fetch('http://localhost:3002/admin/signup',{
                     method:'POST',
                     body:JSON.stringify({
-                        username,
+                        username:email,
                         password
                     }),
                     headers:{
