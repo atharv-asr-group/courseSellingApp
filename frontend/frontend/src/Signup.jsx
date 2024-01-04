5e58c1a488fad789739ad3eba4e9b74957c4e941
unknown <atharv21240@iiitd.ac.in>
2024-01-04 17:02:59 +0530

    writing css from scratch

--- frontend/frontend/src/Signup.jsx
+++ frontend/frontend/src/Signup.jsx
@@ -1,7 +1,21 @@
 function Signup(){
     return (
         <div>
-            Signup
+        <center>
+            <div>
+                Welcome to coursera. Sign up below.
+            </div>
+        <div style={{
+            border:"2px solid black",
+            width:400
+        }}>
+            Username- <input type="text" />
+            <br />
+            Password- <input type="text" />
+            <br />
+            <button>Signup</button>
+        </div>
+        </center>
         </div>
     )
 }
