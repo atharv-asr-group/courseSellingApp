49a9f672bf4e9f5ce7d58a52764c297384b5b4fd
unknown <atharv21240@iiitd.ac.in>
2024-01-04 16:57:11 +0530

    frontend files added

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Signup from './Signup'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hi there</h1>
+      <Signup></Signup>
+    </>
+  )
+}
+
+export default App
