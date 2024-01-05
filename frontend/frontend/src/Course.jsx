ae5ff5327b90f7bebd64e7b6a37b1bd06f1f38b0
unknown <atharv21240@iiitd.ac.in>
2024-01-05 12:22:03 +0530

    added updatecard to update a course details

--- frontend/frontend/src/Course.jsx
+++ frontend/frontend/src/Course.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { CourseCard } from "./Courses";
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 function Course(){
     let {courseId}=useParams();
     const [courses,setCourses]=useState([]);
@@ -28,9 +32,70 @@ function Course(){
         )
     }
     return (
-        <div>
-            {courseId}
+        <div style={{display:'flex' }}>
+            
             <CourseCard course={course}/>
+            <UpdateCard courseId={courseId}/>
+        </div>
+    )
+}
+function UpdateCard(props){
+    const [title,setTitle]=useState('');
+    const [description,setDescription]=useState('');
+    const [link,setLink]=useState('');
+    const [price,setPrice]=useState('');
+    return (
+        <div>
+            
+        <center>
+            <div style={{paddingTop:150}}>
+            </div>
+            <Card variant="outlined" style={{width:400, padding:20}}>
+            <br />
+      <TextField fullWidth={true}  label="title" variant="outlined" 
+      onChange={(e)=>{
+        setTitle(e.target.value);
+      }}/>
+            <br />
+            <TextField fullWidth={true} label="description" variant="outlined" 
+            onChange={(e)=>{
+                setDescription(e.target.value);
+            }}/>
+            <TextField fullWidth={true}  label="image link" variant="outlined" 
+            onChange={(e)=>{
+                setLink(e.target.value);
+            }}/>
+            <TextField fullWidth={true}  label="price" variant="outlined" 
+            onChange={(e)=>{
+                setPrice(e.target.value);
+            }}/>
+
+            <br /><br />
+            <Button variant="contained" 
+            onClick={()=>{
+                console.log(JSON.stringify({
+                    title:title,
+                    description:description,
+                    price:Number(price),
+                    link:link
+                }));
+                fetch('http://localhost:3002/admin/courses/'+props.courseId,{
+                    method:'PUT',
+                    body:JSON.stringify({
+                        title:title,
+                        description:description,
+                        price:Number(price),
+                        imageLink:link
+                    }),
+                    headers:{
+                        'Content-type':'application/json',
+                        'authorization':'Bearer '+localStorage.getItem('token')
+                    }
+                })
+            }}>Update course</Button>
+            <br />
+        </Card>
+        </center>
         </div>
     )
 }
